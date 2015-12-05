7b6798bed0f3dd64af071d3af50cd5b3fe69731a
Gamer1120 <michaelkoopmantest@gmail.com>
2015-12-05 14:27:59 +0100

    BlogConverter finished + emails rearranged

--- src/fileparser/BlogConverter.java
+++ src/fileparser/BlogConverter.java
@@ -3,13 +3,16 @@ package fileparser;
 import java.util.Arrays;
 import java.util.LinkedList;
 
-public class EmailConverter implements DocumentConverter {
+/**
+ * Created by Michael on 5-12-2015.
+ */
+public class BlogConverter implements DocumentConverter {
 
     private LinkedList<Document> trainingSet;
     private LinkedList<Document> testSet;
 
     public static void main(String[] args) {
-        new EmailConverter().readDocuments();
+        new BlogConverter().readDocuments();
     }
 
     @Override
@@ -17,17 +20,17 @@ public class EmailConverter implements DocumentConverter {
         long startTime = System.nanoTime();
         this.trainingSet = new LinkedList<>();
         this.testSet = new LinkedList<>();
-        trainingSet.addAll(FileUtils.readFolder("db/emails/training/ham", "ham"));
-        trainingSet.addAll(FileUtils.readFolder("db/emails/training/spam", "spam"));
-        testSet.addAll(FileUtils.readFolder("db/emails/test", null));
+        trainingSet.addAll(FileUtils.readFolder("db/blogs/training/female", "female"));
+        trainingSet.addAll(FileUtils.readFolder("db/blogs/training/male", "male"));
+        testSet.addAll(FileUtils.readFolder("db/blogs/test", null));
         long endTime = System.nanoTime();
         double duration = Math.round((endTime - startTime) / 1000000000 * 100.0) / 100.0;
-        System.out.println("Successfully read " + trainingSet.size() + " training emails and " + testSet.size() + " test emails in " + duration + " seconds.");
-        /*
+        System.out.println("Successfully read " + trainingSet.size() + " training blogs and " + testSet.size() + " test blogs in " + duration + " seconds.");
+
         System.out.println("Examples: ");
         System.out.println("Training: " + Arrays.toString(trainingSet.get(0).getText()));
         System.out.println("Test: " + Arrays.toString(testSet.get(0).getText()));
-        */
+
     }
 
     @Override
@@ -39,4 +42,5 @@ public class EmailConverter implements DocumentConverter {
     public LinkedList<Document> getTestSet() {
         return testSet;
     }
+
 }
