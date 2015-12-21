10ece92efedeb01d389dbdf5e62d692f41d78e19
meteoorkip <svenkonings@live.nl>
2015-12-21 15:04:32 +0100

    Gui update

--- src/gui/EmailApplierGui.java
+++ src/gui/EmailApplierGui.java
@@ -2,14 +2,18 @@ package gui;
 
 import applying.Applier;
 import applying.EmailApplier;
+import classifier.Document;
 import classifier.naivebayes.MultinomialNaiveBayesClassifier;
 import javafx.application.Application;
 import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.ListView;
 import javafx.scene.control.ScrollPane;
-import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.text.Text;
@@ -20,12 +24,13 @@ import java.util.Collection;
 public class EmailApplierGui extends Application {
     private static final double SIZE = 250d;
     private static final double PADDING = 10d;
-    private static final double MIN_WIDTH = SIZE;
-    private static final double MIN_HEIGHT = SIZE + 2d * PADDING;
+    private static final double MIN_WIDTH = 4d * SIZE + 3d * PADDING;
+    private static final double MIN_HEIGHT = SIZE;
 
     private Applier applier;
 
     private ListView<String> classList;
+    private ComboBox<String> classBox;
     private ListView<String> documentList;
     private ScrollPane scrollPane;
 
@@ -45,12 +50,17 @@ public class EmailApplierGui extends Application {
         scrollPane = generateScrollPane();
         center.getChildren().addAll(classList, documentList, scrollPane);
 
-        AnchorPane bottom = generateBottom();
+        HBox bottom = generateBottom();
+        classBox = generateClassBox();
+        Button trainButton = generateTrainButton();
+        bottom.getChildren().addAll(classBox, trainButton);
 
         root.setCenter(center);
         root.setBottom(bottom);
 
-        Scene scene = new Scene(root, 1280d, 720d);
+        reset();
+
+        Scene scene = new Scene(root);
         primaryStage.setTitle("JokeTeller");
         primaryStage.setScene(scene);
         primaryStage.setMinWidth(MIN_WIDTH);
@@ -58,16 +68,43 @@ public class EmailApplierGui extends Application {
         primaryStage.show();
     }
 
+    private void reset() {
+        setClasses(applier.getDocuments().keySet());
+    }
+
     private void setClasses(Collection<String> classes) {
-        classList.setItems(FXCollections.observableArrayList(classes));
+        ObservableList<String> list = FXCollections.observableArrayList(classes);
+        classList.setItems(list);
+        classBox.setItems(list);
+    }
+
+    private void resetClasses() {
+        setClasses(FXCollections.emptyObservableList());
     }
 
     private void setDocuments(Collection<String> documents) {
         documentList.setItems(FXCollections.observableArrayList(documents));
     }
 
+    private void resetDocuments() {
+        setDocuments(FXCollections.emptyObservableList());
+    }
+
     private void setText(String text) {
         scrollPane.setContent(generateText(text));
+    }
+
+    private void resetText() {
+        setText("");
+    }
+
+    private void updateClassBox() {
+        String selected = classList.getSelectionModel().getSelectedItem();
+        if (selected != null) {
+            classBox.getSelectionModel().select(selected);
+        } else {
+            classBox.getSelectionModel().clearSelection();
+        }
     }
 
     private BorderPane generateRoot() {
@@ -81,8 +118,13 @@ public class EmailApplierGui extends Application {
     private ListView<String> generateClassList() {
         ListView<String> classList = new ListView<>();
         classList.setPrefWidth(SIZE);
-        classList.getSelectionModel().selectedIndexProperty().addListener((observable, oldValue, newValue) -> {
-
+        classList.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+            if (newValue != null) {
+                setDocuments(applier.getDocuments().get(newValue));
+            } else {
+                resetDocuments();
+            }
+            updateClassBox();
         });
         return classList;
     }
@@ -91,7 +133,13 @@ public class EmailApplierGui extends Application {
         ListView<String> documentList = new ListView<>();
         documentList.setPrefWidth(SIZE);
         documentList.getSelectionModel().selectedIndexProperty().addListener((observable, oldValue, newValue) -> {
-
+            int value = newValue.intValue();
+            if (value != -1) {
+                setText(documentList.getItems().get(value));
+            } else {
+                resetText();
+            }
+            updateClassBox();
         });
         return documentList;
     }
@@ -101,18 +149,40 @@ public class EmailApplierGui extends Application {
         scrollPane.setFitToWidth(true);
         scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
         scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
-        scrollPane.setPrefWidth(2d * SIZE);
+        scrollPane.setPrefViewportWidth(2d * SIZE);
         return scrollPane;
     }
 
     private Text generateText(String s) {
         Text text = new Text(s);
-//        text.setWrappingWidth(SIZE);
+        text.setWrappingWidth(2d * SIZE);
         return text;
     }
 
-    private AnchorPane generateBottom() {
-        return new AnchorPane();
+    private HBox generateBottom() {
+        HBox bottom = new HBox(PADDING);
+        bottom.setPadding(new Insets(PADDING));
+        bottom.setAlignment(Pos.CENTER_RIGHT);
+        return bottom;
+    }
+
+    private ComboBox<String> generateClassBox() {
+        return new ComboBox<>();
+    }
+
+    private Button generateTrainButton() {
+        Button button = generateButton("train");
+        button.setOnAction((event -> {
+            String classification = classBox.getSelectionModel().getSelectedItem();
+            String text = ((Text) scrollPane.getContent()).getText();
+            if (classification != null && !"".equals(text)) {
+                Document document = new Document(text, classification);
+                applier.train(document);
+                applier.reClassify();
+                reset();
+            }
+        }));
+        return button;
     }
 
     private Button generateButton(String text) {
